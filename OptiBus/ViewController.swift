16a911e7b127dc5952609b770fd0fe11d0ee65cf
sultan <anfalalatawi@anfals-mbp.kaust.edu.sa>
2018-09-28 06:21:37 +0300

    Initial Commit

---
+++ OptiBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OptiBus
+//
+//  Created by Anfal Alatawi on 28/09/2018.
+//  Copyright © 2018 Anfal Alatawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
